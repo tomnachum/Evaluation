b476181dd3e50b668c62553bf12ba3ed6f9cbaa5
tom-nachum <tom.nachum@gmail.com>
2022-10-23 09:29:14 +0300

    changed folder structure

--- server/DB/create_db/create_pokemons_types_table.sql
+++
@@ -1,8 +0,0 @@
-USE poke_tracker;
-CREATE TABLE pokemons_types(
-    p_id INT,
-    ty_id INT,
-    PRIMARY KEY (p_id, ty_id),
-    FOREIGN KEY(p_id) REFERENCES pokemons(p_id),
-    FOREIGN KEY(ty_id) REFERENCES types(ty_id)
-);
