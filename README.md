a324458ffab3374391f3e6fd9708780e30cc5b89
tom-nachum <tom.nachum@gmail.com>
2022-10-23 12:54:06 +0300

    added README

--- README.md
+++ README.md
@@ -2,19 +2,21 @@
 
 ## Introduction
 
-A full stack project.
+A full stack project, displaying recipes from external API and filter it by several sensitivities.
 
 ## Running instructions
 
 ### Initialize the DB
 
+inside server/DB/create_db
+
 1. run `createDB.sql`
 
 2. run `create_tables.sql`
+
+3. run `initialize_tables.py`
 
 ### run the server
 
 1. run `server.py` inside server folder.
 2. on your browser, go to http://localhost:8000/ to see the website.
-
-## Features
--- server/server.py
+++ server/server.py
@@ -44,24 +44,5 @@ async def get_recipes(response: Response, contains=None, dairy=False, gluten=Fal
     return {"recipes": recipes}
 
 
-# ----------------------------- a request with a body
-# @app.put("/endpoint")
-# async def add_player(request: Request):
-#     body = await request.json()
-
-# ----------------------------- external api call
-# def external_api_call():
-#     pokemon_response = requests.get(
-#         f"https://pokeapi.co/api/v2/pokemon/{p_name}"
-#     ).json()
-
-# ----------------------------- status code
-# @app.put("/get-or-create-task/{task_id}", status_code=status.HTTP_200_OK)
-# def get_or_create_task(task_id: str, response: Response):
-#     if task_id not in tasks:
-#         tasks[task_id] = "This didn't exist before"
-#         response.status_code = status.HTTP_201_CREATED
-#     return tasks[task_id]
-
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8049, reload=True)
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
